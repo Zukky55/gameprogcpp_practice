aaf2b7a955508644d0c352ccef4036079f61efba
Zukky_13 <www.kase@gmail.com>
2020-05-17 16:13:24 +0900

    Add class of Human.

--- Challenge02/Human.cpp
+++ Challenge02/Human.cpp
@@ -1,1 +1,22 @@
 #include "Human.h"
+
+Human::Human(Game* game)
+	:Actor(game)
+{
+	// Create an animated sprite components
+	AnimSpriteComponent* asc = new AnimSpriteComponent(this);
+	std::vector<SDL_Texture*> anims;
+	for (int i = 1; i < 17; i++)
+	{
+		auto str = 
+		game->GetTexture("Asset/Character%u");
+	}
+}
+
+void Human::UpdateActor(float deltaTime)
+{
+}
+
+void Human::ProcessKeyboard(const uint8_t* state)
+{
+}
--- Challenge02/Human.h
+++ Challenge02/Human.h
@@ -1,17 +1,39 @@
 #pragma once
 #include "Actor.h"
+#include"SDL/SDL.h"
+#include "AnimSpriteComponent.h"
 #include<vector>
+#include"Game.h"
 class Human :
 	public Actor
 {
 public:
+	enum class AnimState {
+		// Not use
+		EInvalid = 0,
+		EWalk,
+		EJump,
+		EPunch,
+	};
+	struct AnimMaterial
+	{
+	public:
+		AnimState GetAnimState()const { return mAnimState; }
+		Uint32 GetBegin()const { return mBegin; }
+		Uint32 GetEnd()const { return mEnd; }
+		bool GetIsRepeat()const { return mIsRepeat; }
+	private:
+		AnimState mAnimState;
+		Uint32 mBegin;
+		Uint32 mEnd;
+		bool mIsRepeat;
+	};
+private:
+	std::vector<AnimMaterial> mAnimMaterials;
+
 	Human(class Game* game);
 	void UpdateActor(float deltaTime)override;
 	void ProcessKeyboard(const uint8_t* state);
-	
-private:
-	struct Anim {
-		uint32_t
-	};
+
 };
 
